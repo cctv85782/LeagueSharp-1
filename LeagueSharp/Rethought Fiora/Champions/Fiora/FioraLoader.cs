4a59592129c450e30d6691878ca429c1624b568c
MediaGitHub <bje@live.de>
2016-08-01 16:37:33 +0200

    Fixed Compilling Error

---
+++ LeagueSharp/Rethought Fiora/Champions/Fiora/FioraLoader.cs
@@ -0,0 +1,56 @@
+﻿namespace Rethought_Fiora.Champions.Fiora
+{
+    #region Using Directives
+
+    using RethoughtLib.Bootstraps.Abstract_Classes;
+    using RethoughtLib.FeatureSystem.Implementations;
+
+    using Rethought_Fiora.Champions.Fiora.Modules.Core;
+
+    #endregion
+
+    internal class FioraLoader : LoadableBase
+    {
+        #region Public Properties
+
+        /// <summary>
+        ///     Gets or sets the name that will get displayed.
+        /// </summary>
+        /// <value>
+        ///     The name of the displaying.
+        /// </value>
+        public override string DisplayingName { get; set; } = "Rethought Fiora";
+
+        /// <summary>
+        ///     Gets or sets the internal name.
+        /// </summary>
+        /// <value>
+        ///     The name.
+        /// </value>
+        public override string InternalName { get; set; } = "Fiora";
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     Loads this instance.
+        /// </summary>
+        public override void Load()
+        {
+            var superParent = new SuperParent(this.DisplayingName);
+
+            var coreParent = new Parent("Core");
+
+            var orbwalker = new OrbwalkerModule(superParent.Menu);
+            superParent.AddChildren(orbwalker);
+
+            var spells = new SpellModule();
+            coreParent.AddChildren(spells);
+
+
+        }
+
+        #endregion
+    }
+}
---
+++ LeagueSharp/Rethought Fiora/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Rethought_Fiora
+{
+    #region Using Directives
+
+    using RethoughtLib;
+    using RethoughtLib.Bootstraps.Implementations;
+
+    using Rethought_Fiora.Champions.Fiora;
+
+    #endregion
+
+    internal class Program
+    {
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            RethoughtLib.Instance.Load();
+
+            var bootstrap = new LeagueSharpMultiBootstrap();
+
+            bootstrap.AddModule(new FioraLoader());
+            bootstrap.AddString("Fiora");
+        }
+
+        #endregion
+    }
+}
